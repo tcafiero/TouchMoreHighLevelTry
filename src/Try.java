6977ad6c04b38a4f938763aada1ebd4d0d244c15
Toni Cafiero <toni@cafiero.it>
2013-09-02 21:19:43 +0200

    Release 2.0

--- src/Try.java
+++ src/Try.java
@@ -1,6 +1,7 @@
 //  File : ./main.java
 
 import java.io.*;
+import java.util.*;
 public class Try
 {
 	// Operations
@@ -8,27 +9,67 @@ public class Try
 	public static void main(String[] argv) throws Exception{
 		System.loadLibrary("dsp");
 		Runtime rt = Runtime.getRuntime();
+		boolean exit = false;
+		Scanner s = new Scanner(System.in);
 		dsp.initDSP();
-		dsp.setSource(DR_AUDIO_SOURCES.SRC_TUNER);
-		dsp.setVolume(30);
-		dsp.set_frequency(98100 , TYPE_BAND.FM_BAND, TYPE_SUB_BAND.AM_KW_BAND);		     
-		Thread.sleep(10000);
-		dsp.setSource(DR_AUDIO_SOURCES.SRC_MP3);
-		System.out.println("aplay -D TDM1_O_ENT /home/root/arribba.wav");
-		Process pr = rt.exec("aplay -D TDM1_O_ENT /home/root/arribba.wav");
-		dsp.setVolume(30);
-		BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-
-		String line=null;
-		System.out.println("DR_AUDIO_SOURCES.SRC_MP3");
-
-		while((line=input.readLine()) != null) {
-			System.out.println(line);
+		dsp.setVolume(25);
+			System.out.print("r: Radio");
+			System.out.print("v: Volume");
+			System.out.print("+: Increase Volume");
+			System.out.print("-: Decrease Volume");
+			System.out.print("s: Play sinusoid");
+			System.out.print("e: Exit");
+		while(!exit) 
+		{ 
+			System.out.print("Command: ");
+			String command = s.next();
+			switch (command) {
+			case "e" :
+				exit=true;
+				break;
+			case "r" :
+				dsp.setSource(DR_AUDIO_SOURCES.SRC_TUNER);
+				dsp.doMute();
+				System.out.print("Frequency: ");
+				Integer frequency = s.nextInt();
+				System.out.println("radio Frequency: "+frequency);
+				dsp.set_frequency(frequency , TYPE_BAND.FM_BAND, TYPE_SUB_BAND.AM_KW_BAND);
+				dsp.doDeMute();
+				break;	    		
+			case "v" :
+				System.out.print("Volume: ");
+				Integer volume = s.nextInt();
+				System.out.println("Volume: "+volume);
+				dsp.setVolume(volume);
+				break;	    		
+			case "+" :
+				System.out.print("+");
+				dsp.increase_Volume();
+				break;
+			case "-" :
+				System.out.print("-");
+				dsp.decrease_Volume();
+				break;
+			case "mp3" :
+				dsp.setSource(DR_AUDIO_SOURCES.SRC_MP3);
+				System.out.println("aplay -D TDM1_O_ENT /home/root/arribba.wav");
+				Process pr = rt.exec("aplay -D TDM1_O_ENT /home/root/arribba.wav");
+				dsp.setVolume(25);
+			case "s" :
+				System.out.println("Sinusoid");
+				dsp.playSin(25, 440, true);
+				break;
+			}
 		}
-		int exitVal = pr.waitFor();
-		System.out.println("Exited with error code "+exitVal);
-		dsp.setSource(DR_AUDIO_SOURCES.SRC_TUNER);
-		dsp.setVolume(30);
-		dsp.set_frequency(98100 , TYPE_BAND.FM_BAND, TYPE_SUB_BAND.AM_KW_BAND);		     
+		// edit, lets not hog any cpu time 
+		try 
+		{ 
+			Thread.sleep(50); 
+		} 
+		catch (InterruptedException ex) 
+		{ 
+			// can't do much about it can we? Ignoring  
+		}
+		s.close();
 	}
 }
