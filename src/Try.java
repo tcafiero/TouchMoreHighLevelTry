7bcb25436dab377a3ba0206500ec31d3922633e2
Toni Cafiero <toni@cafiero.it>
2013-09-10 15:13:29 +0200

    Release 3.0

--- src/Try.java
+++ src/Try.java
@@ -8,69 +8,31 @@ public class Try
 
 	public static void main(String[] argv) throws Exception{
 		System.loadLibrary("dsp");
-		Runtime rt = Runtime.getRuntime();
-		boolean exit = false;
-		Scanner s = new Scanner(System.in);
 		dsp.initDSP();
-		dsp.setVolume(25);
+		Infotainment device = new Infotainment();
+		Scanner input = new Scanner(System.in);
 		System.out.println("r: Radio");
-		System.out.println("v: Volume");
+		System.out.println("<integer 0-30>: Volume");
 		System.out.println("+: Increase Volume");
 		System.out.println("-: Decrease Volume");
 		System.out.println("s: Play sinusoid");
 		System.out.println("mp3: Play MP3 file");
-		System.out.println("e: Exit");
-		while(!exit) 
+		System.out.println("x: Exit");
+		while(!device.Exit) 
 		{ 
-			System.out.print("Command: ");
-			String command = s.next();
-			switch (command) {
-			case "e" :
-				exit=true;
-				break;
-			case "r" :
-				dsp.setSource(DR_AUDIO_SOURCES.SRC_TUNER);
-				dsp.doMute();
-				System.out.print("Frequency: ");
-				Integer frequency = s.nextInt();
-				System.out.println("radio Frequency: "+frequency);
-				dsp.set_frequency(frequency , TYPE_BAND.FM_BAND, TYPE_SUB_BAND.AM_KW_BAND);
-				dsp.doDeMute();
-				break;	    		
-			case "v" :
-				System.out.print("Volume: ");
-				Integer volume = s.nextInt();
-				System.out.println("Volume: "+volume);
-				dsp.setVolume(volume);
-				break;	    		
-			case "+" :
-				System.out.print("+");
-				dsp.increase_Volume();
-				break;
-			case "-" :
-				System.out.print("-");
-				dsp.decrease_Volume();
-				break;
-			case "mp3" :
-				dsp.setSource(DR_AUDIO_SOURCES.SRC_MP3);
-				System.out.println("aplay -D TDM1_O_ENT /home/root/arribba.wav");
-				Process pr = rt.exec("aplay -D TDM1_O_ENT /home/root/arribba.wav");
-				dsp.setVolume(25);
-			case "s" :
-				System.out.println("Sinusoid");
-				dsp.playSin(25, 440, true);
-				break;
+			System.out.print("input: ");
+			if (input.hasNextInt())
+				device.UseValue(input.nextInt());
+			else device.Command(input.next());
+			try 
+			{ 
+				Thread.sleep(50); 
+			} 
+			catch (InterruptedException ex) 
+			{ 
+				// can't do much about it can we? Ignoring  
 			}
 		}
-		// edit, lets not hog any cpu time 
-		try 
-		{ 
-			Thread.sleep(50); 
-		} 
-		catch (InterruptedException ex) 
-		{ 
-			// can't do much about it can we? Ignoring  
-		}
-		s.close();
+		input.close();
 	}
 }
